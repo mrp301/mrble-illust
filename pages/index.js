842025cc2e9a716e2464e1d156efee63397f1599
hishinuma616 <hishinuma.yuta@a-tm.co.jp>
2021-04-16 01:01:07 +0900

    init

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -7,6 +8,8 @@ export default function Home() {
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      <Link href="hoge">hoge</Link>
 
       <main>
         <h1 className="title">
