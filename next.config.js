b0f453fb24bece23f6c66eb79fcffba88fde7ec9
hishinuma616 <dnbwoodleaves3616@gmail.com>
2021-10-04 00:36:10 +0900

    buildエラー修正

--- next.config.js
+++ next.config.js
@@ -14,6 +14,6 @@ module.exports = {
     return config;
   },
   images: {
-    domains: ["firebasestorage.googleapis.com", isDev && "localhost"],
+    domains: ["firebasestorage.googleapis.com"],
   },
 };
