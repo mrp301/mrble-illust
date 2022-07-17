f47e814e2e4fd96a0f1113a5442bda70a2ed945c
hishinuma616 <dnbwoodleaves3616@gmail.com>
2022-07-18 02:30:28 +0900

    add OGP

--- components/CommonHead.tsx
+++ components/CommonHead.tsx
@@ -14,6 +14,23 @@ export const CommonHead: FC<Props> = ({ title }) => {
         name="viewport"
         content="width=device-width, initial-scale=1.0, user-scalable=no"
       ></meta>
+      <meta name="viewport" content="initial-scale=1, width=device-width" />
+      <meta name="description" content="過去に描いた絵や同人誌などをまとめたサイト" />
+      <meta property="og:url" content="https://mrble-illustration-mrp301.vercel.app/" />
+      <meta property="og:type" content="website" />
+      <meta
+        property="og:description"
+        content="過去に描いた絵や同人誌などをまとめたサイト"
+      />
+      <meta property="og:title" content="mrble-illustration" />
+      <meta
+        property="og:image"
+        content="https://mrble-illustration-mrp301.vercel.app/images/ogp.jpg"
+      />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content="@tubdaka616" />
+      <meta property="og:site_name" content="mrble-illustration" />
+      <meta property="og:locale" content="ja_JP" />
     </Head>
   );
 };
